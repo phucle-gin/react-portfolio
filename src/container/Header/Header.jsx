bf57fcd9759fa54697cd322bce154bd9f511f420
phucle-gin <hoangphuc2832000@gmail.com>
2022-03-07 00:11:09 +1100

    Fixing background color and circle svg

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) =>(
+        {[images.react, images.javascript, images.sass].map((circle, index) =>(
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
             <img src={circle} alt="circle" />
             </div>
