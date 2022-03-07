5e506bd2ffc046207838dbb0b755d23dfcdce602
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-03-08 02:01:20 +1100

    Fixing minor stuffs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -11,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <a href={`/#`}><h3>Phuc Le</h3></a>
       </div>
       <ul className="app__navbar-link">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
