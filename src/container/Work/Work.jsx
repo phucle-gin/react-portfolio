226076eabb271b3ce44c7f3455ea111fbba76b56
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-03-10 02:03:26 +1100

    Fixing the layout of the work section

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,49 +1,69 @@
-import React, {useState, useEffect} from 'react'
-import { AiFillEye, AiFillGithub} from 'react-icons/ai';
-import {AppWrap} from '../../wrapper';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
-import "./Work.scss";
 import { images } from '../../constants';
+import { AppWrap } from '../../wrapper';
+import './Work.scss';
 const work = [
-  {title: 'Modern UI/UX design', description: 'A Modern UI/UX Portfolio Website', imgUrl:images.about01, tags:['UI/UX', 'All']
-  },
+  {title: 'Web Development', description: 'I am a good web developer.', imgUrl:images.about01, tags:['UI/UX']},
+  {title: 'Web Development', description: 'I am a good web developer.', imgUrl:images.about01, tags:['UI/UX']},
+  {title: 'Web Development', description: 'I am a good web developer.', imgUrl:images.about01, tags:['UI/UX']},
+  {title: 'Web Development', description: 'I am a good web developer.', imgUrl:images.about01, tags:['UI/UX']},
 ];
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState('All');
-  const [animateCard, setAnimateCard] = useState({y : 0, opacity: 1});
   const [works, setWorks] = useState([]);
-  const [filterWorks, setfilterWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
   useEffect(() => {
     const query = '*[_type == "works"]';
-  }, [])
-  
+  }, []);
+
   const handleWorkFilter = (item) => {
-    
-  }
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+
   return (
     <>
-    <h2 className='head-text'>My Creative <span>Work</span></h2>
-    <div className='app__work-filter'>
-      {['UI/UX','Web App','Mobile App', 'React JS','All'].map((item,index) => (
-        <div
-        key= {index}
-        onClick={() => handleWorkFilter(item)}
-        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' :''}`}
-        >
-          {item}
-          </div>
-      ))}
-          <motion.div
-          animate={animateCard}
-          transition={{duration: 0.5, delayChildren: 0.5}}
-          className='app__work-work'
+      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
           >
-          {work.map((work,index) => (
-            <div className='app__work-item app__flex' key={index}>
-              <div className='app__work-img app__flex'>
-                <img src={work.imgUrl} alt="work.name" />  
-                
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-work"
+      >
+        {work.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={work.imgUrl} alt={work.name} />
+
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
@@ -71,23 +91,21 @@ const Work = () => {
                   </motion.div>
                 </a>
               </motion.div>
-              </div>  
+            </div>
 
-                <div className="app__work-content app__flex">
-                <h4 className="bold-text">{work.title}</h4>
-                <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
 
-                <div className="app__work-tag app__flex">
-                  <p className="p-text">{work.tags[0]}</p>
-                </div>
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
               </div>
             </div>
-          ))}
-
-          </motion.div>
-          
-    </div>
+          </div>
+        ))}
+      </motion.div>
     </>
-  )
-}
-export default Work
+  );
+};
+
+export default AppWrap(Work, 'work')
