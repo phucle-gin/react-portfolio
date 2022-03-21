0bc52025ceec458be35a9f3cdbced2f7771ed8eb
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-03-21 12:19:06 +1100

    Adding footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import "./Footer.scss";
 
+import {images} from '../../constants';
+import {AppWrap, MotionWrap} from '../../wrapper';
+
 const Footer = () => {
   return(
-    <div>Footer</div>
+    <>
+      <h2 className='head-text'>
+        Take a Coffe & Chat with me 
+      </h2>
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt="email" />
+          <a href="mailto:phucnh.le@gmail.com" className='p-text'>phucnh.le@gmail.com</a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: 0423851354" className='p-text'>0423851354</a>
+        </div>
+      </div>
+      
+      <div className='app__footer-form app__flex'>
+        <div className='app__flex'>
+          <input className='p-text' type="text" placeholder='Your Name' value={name} onChange={handleChangeInput} name='name'/>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+)
