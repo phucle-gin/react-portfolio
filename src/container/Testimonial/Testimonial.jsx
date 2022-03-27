85a56d5775f3273d643baeb292179346f8a19420
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-03-27 23:39:47 +1100

    Adding testimonial section

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { motion } from 'framer-motion';
+import {images} from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import "./Testimonial.scss";
-
+const [brands, setbrands] = useState([]);
+const [testimonials, tetTestimonials] = useState([]);
+const [currentIndex, setcurrentIndex] = useState([]);
 const Testimonial = () => {
   return (
     <div>Testimonial</div>
   )
 }
 
-export default Testimonial
+
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'), 'testimonial', 'app__whitebg'
+)
