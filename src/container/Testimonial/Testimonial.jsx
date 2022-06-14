48e5ac447b4c02ca903a71c8c418023410871f6d
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-06-15 09:12:02 +1000

    Adding styles to testimonial section and removing unnecessary parts

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -3,16 +3,14 @@ import { motion } from 'framer-motion';
 import {images} from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import "./Testimonial.scss";
-import { HiChevronLeft, HiChevronRight  } from 'react-icons/hi';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 const testimonial =[
   {name: 'Phoenix', feedback:'He is an amazing developer', icon: images.react},
-  {name: 'Aryan', icon: images.sass},
-  {name: 'Naveen', icon: images.git},
+  {name: 'Aryan',feedback:'He is an amazing developer', icon: images.sass},
+  {name: 'Naveen', feedback:'He is an amazing developer',icon: images.git},
 ];
 
 const Testimonial = () => {
-    const [brands, setbrands] = useState([]);
-  const [testimonials, tetTestimonials] = useState([]);
   const [currentIndex, setcurrentIndex] = useState(0);
   const handleClick = (index) => {
     setcurrentIndex(index);
@@ -32,30 +30,24 @@ const Testimonial = () => {
               </div>
           </div>
           <div className='app__testimonial-btns app__flex'>
-            <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonial.length - 1 : currentIndex - 1)}>
-            <HiChevronLeft />
-            </div>
-            <div className='app__flex' onClick={() => handleClick(currentIndex === testimonial.length - 1 ? 0 : currentIndex + 1)}>
-            <HiChevronRight />
-            </div>
+              <div 
+                className='app__flex' 
+                onClick={() => handleClick(currentIndex === 0 ? testimonial.length - 1 : currentIndex - 1)}
+              >
+              <HiChevronLeft />
+              </div>
+              <div 
+                className='app__flex' 
+                onClick={() => handleClick(currentIndex === testimonial.length - 1 ? 0 : currentIndex + 1)}
+              >
+              <HiChevronRight />
+              </div>
           </div>
         </>
       )}
-      <div className='app__testimonial-brands app__flex'>
-        {brands.map((brand) => (
-          <motion.div
-          whileInView={{opacity: [0,1]}}
-          transition={{duration: 0.5, type: 'tween'}}
-          key={brand._id}
-          >
-            <img src={brand.img} alt={brand.name} />  
-          </motion.div>
-        ))}
-      </div>
     </>
     )
 }
-
 
 export default AppWrap(
   MotionWrap(Testimonial, 'app__testimonial'), 'testimonial', 'app__primarybg'
