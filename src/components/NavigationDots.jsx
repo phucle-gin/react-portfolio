3e07ea76f3dd0db8c171bd69e2727cf7d6e0380f
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-07-30 02:54:09 +1000

    Adding styles to header

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -8,7 +8,7 @@ const NavigationDots = ({active}) => {
                   href={`#${item}`} 
                   key={item + index}
                   className='app__navigation-dot'
-                  style={active === item ? {backgroundColor: '#8389cd'} :{backgroundColor: '#bbdcfd	'}}
+                  style={active === item ? {backgroundColor: '#8389cd'} :{backgroundColor: '#bbdcfd'}}
                   > </a>
               ))}
     </div>
