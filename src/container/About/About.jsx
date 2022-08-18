a725649c0359065ae5f35049a5a85fa999af1f67
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-08-18 16:26:52 +1000

    Making changes to about image

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { motion } from 'framer-motion';
 import {images} from '../../constants';
 import { AppWrap , MotionWrap} from '../../wrapper';
+import Tilty from 'react-tilty';
+
 import "./About.scss"
 const abouts = [
   {title: 'Web Development', description: 'I am a good web developer.', imgUrl:images.about01},
@@ -12,10 +14,35 @@ const abouts = [
 const About = () => {
   return (
     <>
+    {/* <a href="#" class="tilter tilter--1">
+					<figure class="tilter__figure">
+						<img class="tilter__image" src="img/1.jpg" alt="img01" />
+						<div class="tilter__deco tilter__deco--shine"><div></div></div>
+						<figcaption class="tilter__caption">
+							<h3 class="tilter__title">Tanya Bondesta</h3>
+							<p class="tilter__description">Toronto</p>
+						</figcaption>
+						<svg class="tilter__deco tilter__deco--lines" viewBox="0 0 300 415">
+							<path d="M20.5,20.5h260v375h-260V20.5z" />
+						</svg>
+					</figure>
+				</a> */}
       <div className='app__about-owner app__flex'>
-        <div className='app__about-image'>
-            <img src={images.about04} alt="owner_image" style={{resizeMode:'cover'}} width="300" height="300" sizes="(min-width: 300px) 300px, 100vw"/>  
+        <Tilty style={{ transformStyle: 'preserve-3d' }}>
+          <div  className="app__tilter">
+          <figure className='app__about-image' style={{ transform: 'translateZ(30px)' }}>
+              <img src={images.about04} alt="owner_image"app__tilter-deco width="300" height="300" sizes="(min-width: 300px) 300px, 100vw"/> 
+              <div className="app__tilter-deco app__deco-shine"><div></div></div> 
+              <figcaption className="app__tilter-caption">
+                <h3 className="app__tilter-title">Phuc Le</h3>
+                <p className="app__tilter-description">Viet Nam</p>
+						  </figcaption>
+            <svg className="app__tilter-deco app__deco-lines" viewBox="0 0 300 300">
+							<path d="M20.5,20.5h260v375h-260V20.5z" />
+						</svg>
+          </figure>
           </div>
+        </Tilty>
           <div className='app__about-content'>
             <h2 className='head-text' >
             About Me</h2>
