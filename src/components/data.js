d3415571525fe1dd71c1bb9910ef2228f493c179
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-10-29 19:55:03 +1100

    fixing somecontents

--- src/components/data.js
+++ src/components/data.js
@@ -7,13 +7,13 @@ const data =[
   },
   {
     name:'Freelance Software developer', 
-    description: 'Remotely assisted interns in internal web functions and produced a basic user-friendly mobile app using the React Native framework, Stripe, UI Materials, and Typescript.', 
+    description: 'Remotely assisted and maintained codebase in internal web functions and produced a basic user-friendly mobile app using the React Native framework, Stripe, UI Materials, and Typescript.', 
     company: 'ATM ONLINE VN',
     experiece: '2021',
   },
   {
-    name:'Software Developer', 
-    description: 'Developed a responsive website in Wordpress by incorporating additional HTML + CSS code and a customised theme. Plugins that are kept up to date include spam protection, SEO optimisation for content blogs, and advanced backup plans.', 
+    name:'Studio Developer', 
+    description: 'Worked with a team of two designers to build a coaching website intergrated with content blogs for Queen of My University , an ambitious startup originating from Melbourne.', 
     company: 'Queen Of My Universe', 
     experiece: '2022'
   },
