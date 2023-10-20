6a87a1ab5a0a6a4b6e23f59a7d0125463351fd30
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2023-10-21 01:21:44 +1100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,7 @@ module.exports = {
         ],
       },
       {
-        test: /.scss$/,
+        test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
