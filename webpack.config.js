ace54368cbbc626c3d068caf856045cb56c70cea
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2023-10-22 13:22:54 +1100

    Changed logo and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -108,7 +108,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       template: "./public/index.html",
-      filename:'index.html',
+      filename:'./index.html',
+      favicon: './public/favicon.ico'
   }),
   
   ],
